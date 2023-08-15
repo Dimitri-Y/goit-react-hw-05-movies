ade1f74c3340d956d19df4345639ec2cae0c9be9
Dimitri-Y <dimoon.bond@gmail.com>
2023-08-15 11:51:14 +0300

    Update CatalogItem.jsx

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -4,10 +4,9 @@ import { useLocation, Link } from 'react-router-dom';
 
 const CatalogItem=({title,id})=>{
     const location = useLocation();
-    const to = location.pathname==="/"?`movies/${id}`:`${id}`;
     return(
         <li className={title?css["CatalogItem"]:css["CatalogItem-disabled"]}>
-            {<Link to={to} state={{ from: location }}>{title}</Link>}
+            {<Link to={`/movies/${id}`} state={{ from: location }}>{title}</Link>}
         </li>);
 }
 CatalogItem.propTypes ={
